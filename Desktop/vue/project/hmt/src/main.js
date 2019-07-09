9b474b2cca1f9a170780480cabb5f77704cc4489
hha <1990707901@qq.com>
2019-07-09 16:44:31 +0800

    initall

---
+++ Desktop/vue/project/hmt/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+//职责1:导入项目需要依赖的资源(包  css....)
+//职责2 :初始化根实例 new vue()选项render作用是把某一个App组件渲染在你指定的容器(.$mount('#app))内
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
